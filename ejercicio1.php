f34b4751e1cabf3366d92b199e43f5fe357ee25f
OscarOrtega-200 <oscarfernandoortegarojas@gmail.com>
2024-06-24 22:08:12 -0500

    cargando ejercicio #1 completo y funcional (inicio de sesión)

--- ejercicio1.php
+++ ejercicio1.php
@@ -7,5 +7,22 @@ $users = array(
     "Luisa Campos" => "luisa123",
     "Mayer Manjarrez" => "mayer23"
 );
+// se solicita al usuario que ingrese su usuario y contraseña
+echo "Ingrese su usuario: ";
+$usuario = readline();
+
+echo "Ingrese su contraseña: ";
+$password = readline();
+
+// Verificamos si el usuario suministrado existe
+if (array_key_exists($usuario, $users)) {
+    $mensaje = match (true) {
+        $password == $users[$usuario] => "¡Bienvenid@ a la huerta ecologica $usuario!",
+        default => "Contraseña incorrecta",
+    };
+    echo "$mensaje \n";
+} else {
+    echo "Usuario no encontrado\n";
+}
 ?>
 
