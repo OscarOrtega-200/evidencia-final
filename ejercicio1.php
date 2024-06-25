d1d825f2384bbe4b4e5b8dc75962206ebc2dcdc7
OscarOrtega-200 <oscarfernandoortegarojas@gmail.com>
2024-06-24 21:55:50 -0500

    cargando ejercicio #1 con la base de datos del inicio de secion

---
+++ ejercicio1.php
@@ -0,0 +1,11 @@
+<?php
+// Base de datos con usuarios y contraseñas
+$users = array(
+    "Einer Sanchez" => "einer123",
+    "Anyi Hortua" => "anyi123",
+    "Oscar Ortega" => "oscar123",
+    "Luisa Campos" => "luisa123",
+    "Mayer Manjarrez" => "mayer23"
+);
+?>
+
