2c60a3196784fe768efe4313b0d9625c76fec55b
OscarOrtega-200 <oscarfernandoortegarojas@gmail.com>
2024-06-25 21:40:25 -0500

    registro e inicio de sesion para personal nuevo

--- registro.php
+++ registro.php
@@ -1,4 +1,37 @@
 <?php
+// Base de datos con usuarios y contraseñas
+$usuario = array();
+
+// Función para crear un nuevo usuario
+function crearUsuario($usuario) {
+    echo "Crear un nuevo usuario:\n";
+    echo "Ingrese su nombre de usuario: ";
+    $nuevousuario = readline();
+    echo "Ingrese su contraseña: ";
+    $nuevacontraseña = readline();
+    $usuario[$nuevousuario] = $nuevacontraseña;
+    echo "Usuario creado con éxito!\n";
+    return $usuario;
+}
+
+// Función para iniciar sesión
+function iniciarSesion($usuario) {
+    echo "Iniciar sesión:\n";
+    echo "Ingrese su usuario: ";
+    $usuario1 = readline();
+    echo "Ingrese su contraseña: ";
+    $contraseña = readline();
+    if (array_key_exists($usuario1, $usuario)) {
+        if ($contraseña == $usuario[$usuario1]) {
+            echo "¡Bienvenid@ a la huerta ecologica $usuario1!\n";
+        } else {
+            echo "Contraseña incorrecta\n";
+        }
+    } else {
+        echo "Usuario no encontrado\n";
+    }
+}
+
 // Menú principal para iniciar sesion o registro
 while (true) {
     echo "¿Qué deseas hacer?\n";
@@ -8,10 +41,10 @@ while (true) {
     $opcion = readline("Elija una opción: ");
     switch ($opcion) {
         case "1":
-            $users = crearUsuario($users);
+            $usuario = crearUsuario($usuario);
             break;
         case "2":
-            iniciarSesion($users);
+            iniciarSesion($usuario);
             break;
         case "3":
             exit;
